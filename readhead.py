ea647ae50e30e0e624fbaf435dcb6e201e932910
Yu Mengchi <2246782556ymc@gmail.com>
2018-10-18 21:34:30 +0800

    add data

---
+++ readhead.py
@@ -0,0 +1,11 @@
+﻿import os
+filename=r"C:\Users\22467\Desktop\腾讯2018广告算法大赛\preliminary_contest_data\userFeature.data"
+f=open(filename)
+index=0
+for line in f.readlines():
+	index+=1
+	line=line.replace("\n","")
+	print(line)
+	if index==100:
+		break
+f.close()
